35b6cce4bc91703204c0aeeafc9dda6b29464407
esraaehab333 <esraaehabb333@gmail.com>
2026-02-03 17:18:34 +0200

    Update animation with Lottie file

--- app/src/main/java/com/example/mealplanner/SplashFragment.java
+++ app/src/main/java/com/example/mealplanner/SplashFragment.java
@@ -10,41 +10,21 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashFragment extends Fragment {
-
-    ImageView logo;
-    TextView appName, subTitle;
-
+    LottieAnimationView lottieSplash;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_splash, container, false);
-
-        logo = view.findViewById(R.id.imageView2);
-        appName = view.findViewById(R.id.app_name_txtView);
-        subTitle = view.findViewById(R.id.subtitle_app_txtView);
-
-        Animation logoAnim = AnimationUtils.loadAnimation(getContext(), R.anim.logo_animation);
-        Animation fadeIn = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
-
-        logo.startAnimation(logoAnim);
-
-        appName.setAlpha(0f);
-        subTitle.setAlpha(0f);
-
-        appName.animate().alpha(1f).setDuration(800).setStartDelay(600).start();
-        subTitle.animate().alpha(1f).setDuration(800).setStartDelay(1000).start();
+        lottieSplash = view.findViewById(R.id.lottieSplash);
+        lottieSplash.playAnimation();
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             NavHostFragment.findNavController(this)
                     .navigate(R.id.action_splashFragment_to_placeholder);
-        }, 2500);
-
+        }, 4500);
         return view;
     }
+
 }
